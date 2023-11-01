1751b54b5b0c3510c3cf27088a5c17489a1939f8
CWalter50 <walt50@gmail.com>
2023-11-01 15:58:13 -0400

    Add files via upload

---
+++ Arduino/RFIDSpiderServo.ino
@@ -0,0 +1,67 @@
+// used this library for rfid.... https://github.com/ArcaEgeCengiz/RFID
+// used this article to get it to work with the servo... just adjusted for LED
+// Found at this article... https://create.arduino.cc/projecthub/Arca_Ege/arduino-rfid-servo-box-4361f1
+
+#include <SPI.h>
+#include <RFID.h>
+
+#include <Servo.h>
+
+Servo myservo;
+
+RFID rfid(10, 9);
+
+boolean card;
+
+void setup() {
+
+
+  Serial.begin(9600);
+  SPI.begin();
+  rfid.init();
+  
+ // myservo.attach(6);
+
+Serial.println("Connecting");
+
+ delay(1000); // sanity delay
+  
+
+}
+
+void loop() {
+ if (rfid.isCard())
+  {
+      Serial.println("Found a Card");
+  
+  
+      myservo.attach(6);
+      // continuos servo: write 90: Dont move. Write 180 turns Left or Right, Write 0 turns the other way. THe delay is how long you turn for.
+       myservo.write(140); 
+       delay(1000);
+
+      myservo.write(70);
+      delay(1000);
+
+      myservo.write(-70);
+      delay(1000);
+
+
+
+      
+       
+      //  myservo.write(60);
+      //  delay(850);
+      
+    
+     // rfid.halt();       
+      
+      myservo.detach();
+  }
+  else
+  {
+    // stop it from moving
+    myservo.detach();
+     }
+
+}
